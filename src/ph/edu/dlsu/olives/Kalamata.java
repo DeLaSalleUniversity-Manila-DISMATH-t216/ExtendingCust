ca7dc64e04683744607d2777a5178f33cbb65272
aenoncunanan <aenon_cunanan2@yahoo.com>
2016-02-22 22:25:55 +0800

    Initial commit

---
+++ src/ph/edu/dlsu/olives/Kalamata.java
@@ -0,0 +1,12 @@
+package ph.edu.dlsu.olives;
+
+public class Kalamata extends Olive {
+
+    public Kalamata() {
+        super(2);
+        this.name = "Kalamata";
+        this.flavor = "Grassy";
+        this.color = Olive.BLACK;
+    }
+
+}
